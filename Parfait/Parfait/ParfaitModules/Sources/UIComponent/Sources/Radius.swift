bcba000ceca217a7a10b0446616ce8a7658dc362
namsoo <are34@naver.com>
2026-07-12 15:48:52 +0900

    [feat]: radius추가 (#15)

--- Parfait/Parfait/ParfaitModules/Sources/UIComponent/Sources/Radius.swift
+++ Parfait/Parfait/ParfaitModules/Sources/UIComponent/Sources/Radius.swift
@@ -7,7 +7,22 @@
 
 import Foundation
 
-/// 파르페 디자인 코너 radius 토큰 (Figma Radius/*)
+/// 파르페 디자인 radius 토큰 
 public enum Radius {
+    /// 4px
+    public static let xsmall: CGFloat = 4
+    /// 8px
     public static let small: CGFloat = 8
+    /// 12px
+    public static let medium1: CGFloat = 12
+    /// 16px
+    public static let medium2: CGFloat = 16
+    /// 24px
+    public static let large: CGFloat = 24
+    /// 32px
+    public static let xlarge1: CGFloat = 32
+    /// 48px
+    public static let xlarge2: CGFloat = 48
+    /// 99999px (max)
+    public static let round: CGFloat = 99999
 }
